920c69f278c83443df2a43ab95c4139fe49bed58
henryWolst <henry@wolst.de>
2024-10-25 14:37:05 +0200

    Bearbeitet

---
+++ demo/src/main/java/edu/fra/uas/demo/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package edu.fra.uas.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value="demo", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return "<h1>Hello World Spring Boot!<h1>";
+    }
+    
+
+}
